535c50f9ced96fe0aaa38b0e5b28449c89dbcbf6
Mark Lee <mlee.aus@gmail.com>
2024-06-07 12:34:01 +1000

    wip: add tls cert paths to config

--- config/config.go
+++ config/config.go
@@ -4,23 +4,27 @@ import (
 	"os"
 )
 
+type Config struct {
+	AppName       string
+	Port          string
+	TLSPublicKey  string
+	TLSPrivateKey string
+}
+
+func NewConfig() *Config {
+	config := Config{
+		AppName:       getEnv("APPNAME", "NoName"),
+		Port:          getEnv("PORT", "8443"),
+		TLSPublicKey:  getEnv("TLS_PUBLICKEY", ""),
+		TLSPrivateKey: getEnv("TLS_PRIVATEKEY", ""),
+	}
+
+	return &config
+}
+
 func getEnv(key, defaultValue string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
 	return defaultValue
-}
-
-type Config struct {
-	AppName string
-	Port    string
-}
-
-func NewConfig() *Config {
-	config := Config{
-		AppName: getEnv("APPNAME", "NoName"),
-		Port:    getEnv("PORT", "8443"),
-	}
-
-	return &config
 }
