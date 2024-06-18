cfb7ed79838e0415483099a2aafc48e46c6e1a01
Mark Lee <mlee.aus@gmail.com>
2024-06-18 14:47:03 +1000

    wip: add middleware to set security headers and disable cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -40,6 +40,7 @@ func main() {
 		middleware: []Middleware{
 			requestLogger,
 			secureHeaders,
+			disableCache,
 		},
 	}
 
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -22,10 +22,25 @@ func (app *application) use(handler http.HandlerFunc, m ...Middleware) http.Hand
 	return wrappedHandler
 }
 
+func disableCache(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "private, no-cache, no-store, must-revalidate, proxy-revalidate")
+		w.Header().Set("Expires", "0")
+		w.Header().Set("Surrogate-Control", "max-age=0")
+		println("cache disabled")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
-		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-DNS-Prefetch-Control", "off")
+		w.Header().Set("X-Download-Options", "noopen")
+		w.Header().Set("Strict-Transport-Security", "max-age=15552000; includeSubDomains")
+
 		println("secure headers ran")
 		next.ServeHTTP(w, r)
 	})
