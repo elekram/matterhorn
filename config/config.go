e2d585aad49e074c67f96cc0065bbfdbbdcbbd55
Mark Lee <mlee.aus@gmail.com>
2024-06-21 15:47:46 +1000

    feat: check for missing environment variables and log.fatal if missing

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package environment
 
 import (
+	"log"
 	"os"
+	"strconv"
 )
 
 type Config struct {
@@ -9,22 +11,43 @@ type Config struct {
 	Port          string
 	TLSPublicKey  string
 	TLSPrivateKey string
+	SessionName   string
+	SessionSecret string
+	SessionSecure bool
+	SessionMaxAge string
 }
 
 func NewConfig() *Config {
+	secure, err := strconv.ParseBool(getEnv("SESSION_SECURE", ""))
+	if err != nil {
+		log.Fatal("Error: ession environment variable")
+	}
+
 	config := Config{
 		AppName:       getEnv("APPNAME", "NoName"),
 		Port:          getEnv("PORT", "8443"),
 		TLSPublicKey:  getEnv("TLS_PUBLICKEY", ""),
 		TLSPrivateKey: getEnv("TLS_PRIVATEKEY", ""),
+		SessionName:   getEnv("SESSION_NAME", "DummySessionName"),
+		SessionSecret: getEnv("SESSION_SECRET", ""),
+		SessionSecure: secure,
+		SessionMaxAge: getEnv("SESSION_MAXAGE", ""),
 	}
 
 	return &config
 }
 
 func getEnv(key, defaultValue string) string {
-	if value, ok := os.LookupEnv(key); ok {
+	value, ok := os.LookupEnv(key)
+
+	if !ok {
+		log.Printf("Key: %s", key)
+		log.Fatal("missing key")
+	}
+
+	if len(value) > 0 {
 		return value
 	}
+
 	return defaultValue
 }
