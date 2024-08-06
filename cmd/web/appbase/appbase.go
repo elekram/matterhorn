0b6a332bde32a2c3930a920fbb191a6f8ebaac71
Mark Lee <mlee.aus@gmail.com>
2024-08-06 15:46:42 +1000

    wip: move base session handlers to main package and own file 'sessionhandlers.go'

--- cmd/web/appbase/appbase.go
+++ cmd/web/appbase/appbase.go
@@ -1,96 +1,47 @@
 package appBase
 
 import (
-	"context"
-	"fmt"
-	"io"
+	"html/template"
+	"log"
 	"net/http"
-	"net/url"
-	"strings"
 
-	"github.com/elekram/matterhorn/cmd/web/ui"
 	appcfg "github.com/elekram/matterhorn/config"
-	"go.mongodb.org/mongo-driver/mongo"
+	database "github.com/elekram/matterhorn/db"
 )
 
-func SignIn(cfg *appcfg.ConfigProperties, db *mongo.Client) http.Handler {
+func SignIn(cfg *appcfg.ConfigProperties, failedAuth bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// _appName := env.Config.AppName
-
-		if cfg.DevMode {
-			component := ui.SignIn()
-			component.Render(context.Background(), w)
-			return
+		files := []string{
+			"./cmd/web/templates/appbase-signin.tmpl",
 		}
 
-		component := ui.SignIn()
-		component.Render(context.Background(), w)
-	})
-}
+		ts := template.Must(template.ParseFiles(files...))
 
-func Home(cfg *appcfg.ConfigProperties, db *mongo.Client) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		appName := cfg.AppName
-
-		if cfg.DevMode {
-			component := ui.Layout(ui.DevHead(appName))
-			component.Render(context.Background(), w)
-			return
-		}
-
-		component := ui.Layout(ui.Head(appName))
-		component.Render(context.Background(), w)
-	})
-}
-
-// func Root(w http.ResponseWriter, r *http.Request) {
-// 	appName := cfg.AppName
-
-// 	if cfg.DevMode {
-// 		component := ui.Layout(ui.DevHead(appName))
-// 		component.Render(context.Background(), w)
-// 		return
-// 	}
-
-// 	component := ui.Layout(ui.Head(appName))
-// 	component.Render(context.Background(), w)
-
-// }
-
-func Auth(w http.ResponseWriter, r *http.Request) {
-	pBody := bodyMapper(r.Body)
-
-	for key, value := range pBody {
-		fmt.Println(key, value)
-	}
-
-	w.Write([]byte("Auth post"))
-}
-
-func bodyMapper(b io.ReadCloser) map[string]string {
-	body, err := io.ReadAll(b)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	b.Close()
-
-	bodyMap := map[string]string{}
-	bodyElements := strings.Split(string(body), "&")
-
-	for _, bodyElement := range bodyElements {
-		elementParts := strings.Split(string(bodyElement), "=")
-
-		name := elementParts[0]
-		value := elementParts[1]
-
-		unescapedValue, err := url.QueryUnescape(value)
+		err := ts.Execute(w, failedAuth)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+	})
+}
+
+func Home(cfg *appcfg.ConfigProperties, db database.AppDb) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		files := []string{
+			"./cmd/web/templates/appbase-home.tmpl",
 		}
 
-		bodyMap[name] = unescapedValue
-	}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
 
-	return bodyMap
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+	})
 }
