7c977c6ead6abc59af97af9ed6d6774cc7425e49
Mark Lee <mlee.aus@gmail.com>
2024-08-08 15:47:09 +1000

    wip: add cors support for OPTIONS method and add redirect for signin

--- cmd/web/sessions.go
+++ cmd/web/sessions.go
@@ -23,21 +23,17 @@ var (
 	ErrInvalidValue       = errors.New("invalid cookie value")
 	ErrInvalidCookieValue = errors.New("cookie failed intregity check")
 	ErrBadOAuthRequest    = errors.New("state id check failed")
+	ErrRetrievingProfile  = errors.New("error retrieving profile")
 )
 
-type oAuthIdPool struct {
-	expiry time.Time
-}
-
 type sessionMgr struct {
-	sessionName    string
-	sessiongSecret string
-	maxAge         int
-	useMemoryStore bool
-	secureSession  bool
-	oAuthIdPool    map[string]oAuthIdPool
-	context        session
-	memoryStore    *memoryStore
+	sessionName             string
+	sessiongSecret          string
+	maxAge                  int
+	secureSession           bool
+	oAuthStateParemeterPool map[string]oAuthStateParemeters
+	useMemoryStore          bool
+	memoryStore             *memoryStore
 }
 
 type memoryStore struct {
@@ -48,6 +44,11 @@ type memoryStore struct {
 type session struct {
 	profile googleProfile
 	expiry  time.Time
+}
+
+type oAuthStateParemeters struct {
+	redirectUrl string
+	expiresOn   time.Time
 }
 
 type googleProfile struct {
@@ -62,6 +63,11 @@ type googleProfile struct {
 
 func (s *sessionMgr) manageSession(app *app) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" &&
+			strings.HasPrefix(r.RequestURI, "/static") {
+			addCORSHeaders(w)
+		}
+
 		if strings.HasPrefix(r.RequestURI, "/static") ||
 			strings.HasPrefix(r.RequestURI, "/auth/oauth") {
 			app.router.ServeHTTP(w, r)
@@ -82,38 +88,39 @@ func (s *sessionMgr) manageSession(app *app) http.HandlerFunc {
 				return
 			}
 
-			s.context = context
-
-			if time.Now().After(s.context.expiry) {
-				fmt.Println("session expired")
+			if time.Now().After(context.expiry) {
 				s.destroyCookie(w)
 				app.handlers.handleSignIn.ServeHTTP(w, r)
 				return
 			}
 
-			fmt.Println("session active")
+			if strings.HasPrefix(r.RequestURI, "/oauth2/redirect/google") {
+				postSignInUrl := "/"
+				http.Redirect(w, r, postSignInUrl, http.StatusTemporaryRedirect)
+				return
+			}
+
 			app.router.ServeHTTP(w, r)
 			return
 		}
 
 		if !s.cookieExists(r) &&
-			r.Method == "GET" &&
 			strings.HasPrefix(r.RequestURI, "/oauth2/redirect/google") {
-			stateId := r.FormValue("state")
+			oAuthStateId := r.FormValue("state")
 
-			fmt.Println("2: " + stateId)
-
-			_, keyPresent := s.oAuthIdPool[stateId]
+			_, keyPresent := s.oAuthStateParemeterPool[oAuthStateId]
 			if !keyPresent {
 				http.Error(w, ErrBadOAuthRequest.Error(), http.StatusBadRequest)
 				return
 			}
 
-			defer s.clearIdPool()
+			delete(s.oAuthStateParemeterPool, oAuthStateId)
+			defer s.cleanUpExpiredOAuthParameterIds()
 
-			googleContext, err := HandleOAuthCallback(w, r, app.oAuth2Config)
+			googleContext, err := getGoogleProfile(r, app.oAuth2Config)
 			if err != nil {
-				fmt.Println(err.Error())
+				fmt.Println(err)
+				http.Error(w, ErrRetrievingProfile.Error(), http.StatusServiceUnavailable)
 				return
 			}
 
@@ -130,10 +137,11 @@ func (s *sessionMgr) manageSession(app *app) http.HandlerFunc {
 				expiry:  time.Now().Add(time.Second * time.Duration(s.maxAge)),
 			}
 			s.memoryStore.mu.Unlock()
-			fmt.Println(s.memoryStore.store)
 
 			s.setCookie(w, newSessionId)
-			app.handlers.handleHomePage.ServeHTTP(w, r)
+
+			postSignInUrl := "/"
+			http.Redirect(w, r, postSignInUrl, http.StatusTemporaryRedirect)
 			return
 		}
 
@@ -144,8 +152,37 @@ func (s *sessionMgr) manageSession(app *app) http.HandlerFunc {
 	})
 }
 
-func HandleOAuthCallback(
-	w http.ResponseWriter,
+func newSession(
+	sessionName,
+	sessionSecret,
+	maxAge string,
+	secureSession bool) *sessionMgr {
+
+	ms := memoryStore{
+		store: map[string]session{},
+		mu:    sync.Mutex{},
+	}
+
+	ma, err := strconv.Atoi(maxAge)
+	if err != nil {
+		fmt.Println("New Session Error: maxAge not a number")
+		panic(err)
+	}
+
+	s := sessionMgr{
+		sessionName:             sessionName,
+		sessiongSecret:          sessionSecret,
+		maxAge:                  ma,
+		secureSession:           secureSession,
+		oAuthStateParemeterPool: map[string]oAuthStateParemeters{},
+		useMemoryStore:          true,
+		memoryStore:             &ms,
+	}
+
+	return &s
+}
+
+func getGoogleProfile(
 	r *http.Request,
 	OAuthCfg *oauth2.Config) (googleProfile, error) {
 	g := googleProfile{}
@@ -165,66 +202,24 @@ func HandleOAuthCallback(
 
 	defer resp.Body.Close()
 
-	var response map[string]interface{}
+	var googleResponse map[string]interface{}
 
-	err = json.NewDecoder(resp.Body).Decode(&response)
+	err = json.NewDecoder(resp.Body).Decode(&googleResponse)
 	if err != nil {
 		return g, err
 	}
 
 	g = googleProfile{
-		email:       response["email"].(string),
-		name:        response["name"].(string),
-		family_name: response["family_name"].(string),
-		given_name:  response["given_name"].(string),
-		hd:          response["hd"].(string),
-		id:          response["id"].(string),
-		picture:     response["picture"].(string),
+		email:       googleResponse["email"].(string),
+		name:        googleResponse["name"].(string),
+		family_name: googleResponse["family_name"].(string),
+		given_name:  googleResponse["given_name"].(string),
+		hd:          googleResponse["hd"].(string),
+		id:          googleResponse["id"].(string),
+		picture:     googleResponse["picture"].(string),
 	}
 
 	return g, err
-}
-
-func (s *sessionMgr) clearIdPool() {
-	expiredIds := []string{}
-	for key, val := range s.oAuthIdPool {
-		if time.Now().After(val.expiry) {
-			expiredIds = append(expiredIds, key)
-		}
-	}
-
-	for _, val := range expiredIds {
-		delete(s.oAuthIdPool, val)
-	}
-}
-
-func newSession(sessionName, sessionSecret, maxAge string, secureSession bool) *sessionMgr {
-	ms := memoryStore{
-		store: map[string]session{},
-		mu:    sync.Mutex{},
-	}
-
-	ma, err := strconv.Atoi(maxAge)
-	if err != nil {
-		fmt.Println("New Session Error: maxAge not a number")
-		panic(err)
-	}
-
-	s := sessionMgr{
-		sessionName:    sessionName,
-		sessiongSecret: sessionSecret,
-		maxAge:         ma,
-		secureSession:  secureSession,
-		oAuthIdPool:    map[string]oAuthIdPool{},
-		useMemoryStore: true,
-		memoryStore:    &ms,
-		context: session{
-			profile: googleProfile{},
-			expiry:  time.Time{},
-		},
-	}
-
-	return &s
 }
 
 func (s *sessionMgr) cookieExists(r *http.Request) bool {
@@ -238,7 +233,6 @@ func (s *sessionMgr) cookieExists(r *http.Request) bool {
 }
 
 func (s *sessionMgr) setCookie(w http.ResponseWriter, sessionId string) {
-
 	if s.useMemoryStore {
 		signedValues := signCookie(s.sessionName, sessionId, s.sessiongSecret)
 		encodedCookieValue := base64.URLEncoding.EncodeToString([]byte(signedValues))
@@ -282,38 +276,6 @@ func (s *sessionMgr) getCookie(r *http.Request) (string, error) {
 	return string(authenticCookieValue), nil
 }
 
-func signCookie(cookieName, cookieVal, sessionSecret string) string {
-	hmac := hmac.New(sha256.New, []byte(sessionSecret))
-	hmac.Write([]byte(cookieName))
-	hmac.Write([]byte(cookieVal))
-
-	signature := hmac.Sum(nil)
-	signedCookie := string(signature) + cookieVal
-
-	return signedCookie
-}
-
-func getAuthenticatedCookieVal(cookieName, sessionSecret string, signedCookieValue []byte) ([]byte, error) {
-	if len(signedCookieValue) < sha256.Size {
-		return nil, ErrInvalidCookieValue
-	}
-
-	signature := signedCookieValue[:sha256.Size]
-	value := signedCookieValue[sha256.Size:]
-
-	mac := hmac.New(sha256.New, []byte(sessionSecret))
-
-	mac.Write([]byte(cookieName))
-	mac.Write([]byte(value))
-	expectedSignature := mac.Sum(nil)
-
-	if !hmac.Equal([]byte(signature), expectedSignature) {
-		return nil, ErrInvalidCookieValue
-	}
-
-	return value, nil
-}
-
 func (s *sessionMgr) destroyCookie(w http.ResponseWriter) {
 	sessionName := s.sessionName
 
@@ -334,4 +296,63 @@ func (s *sessionMgr) generateId(length int) string {
 	b := make([]byte, length+2)
 	r.Read(b)
 	return fmt.Sprintf("%x", b)[2 : length+2]
+}
+
+func (s *sessionMgr) cleanUpExpiredOAuthParameterIds() {
+	expiredIds := []string{}
+	for key, parameter := range s.oAuthStateParemeterPool {
+		if time.Now().After(parameter.expiresOn) {
+			expiredIds = append(expiredIds, key)
+		}
+	}
+
+	for _, id := range expiredIds {
+		delete(s.oAuthStateParemeterPool, id)
+	}
+}
+
+func signCookie(cookieName, cookieVal, sessionSecret string) string {
+	hmac := hmac.New(sha256.New, []byte(sessionSecret))
+	hmac.Write([]byte(cookieName))
+	hmac.Write([]byte(cookieVal))
+
+	signature := hmac.Sum(nil)
+	signedCookie := string(signature) + cookieVal
+
+	return signedCookie
+}
+
+func getAuthenticatedCookieVal(
+	cookieName,
+	sessionSecret string,
+	signedCookieValue []byte) ([]byte, error) {
+
+	if len(signedCookieValue) < sha256.Size {
+		return nil, ErrInvalidCookieValue
+	}
+
+	signature := signedCookieValue[:sha256.Size]
+	value := signedCookieValue[sha256.Size:]
+
+	mac := hmac.New(sha256.New, []byte(sessionSecret))
+
+	mac.Write([]byte(cookieName))
+	mac.Write([]byte(value))
+	expectedSignature := mac.Sum(nil)
+
+	if !hmac.Equal([]byte(signature), expectedSignature) {
+		return nil, ErrInvalidCookieValue
+	}
+
+	return value, nil
+}
+
+func addCORSHeaders(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Vary", "Origin")
+	w.Header().Add("Vary", "Access-Control-Request-Method")
+	w.Header().Add("Vary", "Access-Control-Request-Headers")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Accept, token")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 }
